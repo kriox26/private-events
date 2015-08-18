9ba1f2fd70da0b59f25882a712971e87815d73a5
kriox26 <matias.pan26@gmail.com>
2015-08-18 15:56:53 -0300

    add uniqueness and date fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,7 +12,6 @@ class Event < ActiveRecord::Base
   validates :location, presence: true
   validates :description, presence: true, length: { miminum: 30, maximum: 255 }
   validates :date, presence: true
-  validates :time, presence: true
   validate  :event_cant_be_in_the_past
 
 
