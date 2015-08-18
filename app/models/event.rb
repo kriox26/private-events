cf780ef5161950957fa7d1b586e3f86ca94cbaf2
kriox26 <matias.pan26@gmail.com>
2015-08-18 13:08:50 -0300

    add validations to events datetime field

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,6 +10,14 @@ class Event < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 50 }
   validates :location, presence: true
   validates :description, presence: true, length: { miminum: 30, maximum: 255 }
-  validates_datetime :date, on_or_after: lambda { Date.today }, presence: true, message: "Date can't be empty and must be valid."
+  validates :date, presence: true
+  validates :event_cant_be_in_the_past
+
+
+  # Validations
+  private
+	def event_cant_be_in_the_past
+	  self.errors.add(:date, 'Event cant be in the past') if !date.blank? && date > Time.now
+	end
 
 end
