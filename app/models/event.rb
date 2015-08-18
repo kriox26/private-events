74c633663edbf6462990dcff3fa797f847499367
kriox26 <matias.pan26@gmail.com>
2015-08-18 14:59:38 -0300

    add time to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,17 +7,17 @@ class Event < ActiveRecord::Base
   has_many 	 :attendees, through: :invites
 
   # Validations
+  VALID_START_TIME = /\A[0-2][0-3]+:[0-5][0-9]\z/
   validates :title, presence: true, length: { maximum: 50 }
   validates :location, presence: true
   validates :description, presence: true, length: { miminum: 30, maximum: 255 }
   validates :date, presence: true
-  validates :event_cant_be_in_the_past
+  validates :time, presence: true
+  validate  :event_cant_be_in_the_past
 
 
-  # Validations
-  private
-	def event_cant_be_in_the_past
-	  self.errors.add(:date, 'Event cant be in the past') if !date.blank? && date > Time.now
-	end
+  def event_cant_be_in_the_past
+	errors.add(:date, 'can\'t be in the past') if !date.blank? && date < Time.zone.today
+  end
 
 end
