baad37bb99b52f2771dc26b5f0a1190a062cbf75
kriox26 <matias.pan26@gmail.com>
2015-08-18 11:22:51 -0300

    add validations to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,10 @@ class Event < ActiveRecord::Base
   has_many 	 :invites,   foreign_key: :attended_event_id
   has_many 	 :attendees, through: :invites
 
+  # Validations
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :location, presence: true
+  validates :description, presence: true, length: { miminum: 30, maximum: 255 }
+  validates_datetime :date, on_or_after: lambda { Date.current }
+
 end
