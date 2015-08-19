16f8b424193f28a22caae1504add98730cdfac20
kriox26 <matias.pan26@gmail.com>
2015-08-19 12:30:15 -0300

    users can attend/unattend events now

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,28 +1,22 @@
 class InvitesController < ApplicationController
-  before_action :correct_user , only: [ :create , :destroy ]
+  before_action :correct_user , only: [ :create ]
 
   def create
-	@event = Event.find(params[:attended_event_id])
+	@event = Event.find(params[:invite][:attended_event_id])
     current_user.invites.create(attended_event_id: @event.id)
-	respond_to do |format|
-	  format.html { redirect_to @event }
-	  format.js
-	end
+	redirect_to @event
   end
 
   def destroy
 	@invite = Invite.find(params[:id])
 	current_user.invites.destroy(@invite)
-	respond_to do |format|
-	  format.html { redirect_to @event }
-	  format.js
-	end
+	redirect_to :back
   end
 
   private
 
 	def correct_user
-	  !current_user?(User.find(Event.find(params[:id]).creator_id))
+	  !current_user?(User.find(Event.find(params[:invite][:attended_event_id]).creator_id))
 	end
 
 end
